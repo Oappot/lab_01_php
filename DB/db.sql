fb97c8d5ecccfc5179d66b4819b86e6c337fa1ba
oappot <oappot@gmail.com>
2017-11-22 03:19:18 +0300

    test in night
    )))

--- DB/db.sql
+++ DB/db.sql
@@ -14,8 +14,7 @@ GRANT ALL ON orfarile.* TO 'orfarile'@'%';
 CREATE TABLE IF NOT EXISTS users(
     user_id INT(10) NOT NULL AUTO_INCREMENT,
     user_login VARCHAR(20) NOT NULL,
-    user_password VARCHAR(32) NOT NULL,
-    user_mail VARCHAR(50) NOT NULL,
+    user_password VARCHAR(100) NOT NULL,
     PRIMARY KEY (user_id)
 );
 
